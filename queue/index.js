81a495fb664c027869dc76dbd566d5679ac3e22a
Melanie Cohen <melaniebcohen@gmail.com>
2018-04-14 14:57:55 -0700

    queue solution & documentation

--- queue/index.js
+++ queue/index.js
@@ -1,20 +1,40 @@
 'use strict';
 
-module.exports = exports = {};
+const Node = require('./lib/node').QueueNode;
 
-// * Implement `dequeue` as a method of your `Queue`.
+module.exports = exports = {};
 
 exports.Queue = class {
   constructor() {
-    // this.next = null;
-    this.qLength = 0;
     this.first = null;
     this.last = null;
+    this.length = 0;
   }
 
   enqueue(value) {
+    if (!value) throw new Error('No value provided');
+    let newNode = new Node(value, null);
+
+    if (!this.last) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = this.last.next;
+    }
+    this.length++;
+    return this;
   }
 
   dequeue() {
+    if (this.first !== null) {
+      let newFirst = this.first;
+      this.first = this.first.next;
+      this.length--;
+
+      if (this.length === 0) this.last = null;
+      return newFirst;
+    }
+    if (!this.first) throw new Error('cannot dequeue empty queue');
   }
 };
